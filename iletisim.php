88930e14ff77003e60eb34bd63f1e814d873b5a1
mohammedyasinalali <mohammadyasinalali@gmail.com>
2024-05-12 13:00:01 +0300

    edit4

---
+++ iletisim.php
@@ -0,0 +1,9 @@
+<?php
+ echo "Adı:".$_POST['ad'];
+ echo "<br>Soyadı:".$_POST['soyad'];
+ echo "<br>Doğum Tarihi:".$_POST['doğum'];
+ echo "<br>Eğitim Düzeyi:".$_POST['eğitim']; 
+ echo "<br>Email:".$_POST['email'];
+ echo "<br>Cinsiyet:".$_POST['cns'];
+ echo "<br>Kullanıcı Mesajı:".$_POST['mesaj'];
+ ?>
